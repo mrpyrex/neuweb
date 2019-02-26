71e2983d4c319bda47e38f445babe335060089bd
mrpyrex <ndifrkeumoren@gmail.com>
2019-02-26 09:49:36 +0100

    Modified settings

--- neuweb/settings.py
+++ neuweb/settings.py
@@ -157,16 +157,6 @@ CKEDITOR_CONFIGS = {
     },
 }
 
-# Email Settings
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = ''
-
-
 # Local file settings
 
 try:
